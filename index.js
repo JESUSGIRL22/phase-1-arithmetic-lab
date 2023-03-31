eac6ac74c2f8e672d77c190a5d80dbc571ba8caf
techlover <e.rono@outlook.com>
2023-03-31 18:33:24 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Challenge 1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2
+const random = Math.ceil(Math.random() * 100);
+
+// Challenge 3
+const num3 = 40;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Challenge 4
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const max = Math.max(...numbers);
